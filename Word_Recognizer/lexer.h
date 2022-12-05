eca7e7eb8a926d8c3e132af67f5d40e5dcbaa106
Colton Meyers <cmmeyers63@gmail.com>
2022-12-04 17:06:17 -0700

    Somehow managed to become more confused about makefiles. I'll have to try again a little later

---
+++ Word_Recognizer/lexer.h
@@ -0,0 +1,6 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+int add(int x, int y);
+
+#endif // LEXER_H
---
+++ Word_Recognizer/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "lexer.h"
+
+int main(){
+	printf("hello world\n");
+
+	printf("%d", add(1,1));
+
+	return 0;
+}
