47b720dcb21009b0ebe4d98f5345889553e3c61d
Colton Meyers <cmmeyers63@gmail.com>
2022-12-25 22:05:48 -0800

    stupid windows. WHO doesn't have a C compiler in path

--- Word_Recognizer/lexer.h
+++
@@ -1,6 +0,0 @@
-#ifndef LEXER_H
-#define LEXER_H
-
-int add(int x, int y);
-
-#endif // LEXER_H
--- Word_Recognizer/main.c
+++ Word_Recognizer/main.c
@@ -1,10 +1,5 @@
-#include <stdio.h>
-#include "lexer.h"
-
-int main(){
-	printf("hello world\n");
-
-	printf("%d", add(1,1));
-
-	return 0;
+ 
+int main(int argc, char *argv[])
+{
+    return 0;
 }
